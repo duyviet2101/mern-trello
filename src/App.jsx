6e8d48d307c3c3f96faafbd6ebe22ec7f67af155
duyviet2101 <whoisduyviet@gmail.com>
2024-05-27 00:24:04 +0700

    18. layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import { CompareSharp, ThreeDRotation } from '@mui/icons-material'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
@@ -13,6 +7,7 @@ import Box from '@mui/material/Box';
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlined from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Container from '@mui/material/Container'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -60,48 +55,40 @@ function ModeSelect() {
     </FormControl>
   )
 }
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-
-  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
-  // console.log('prefersDarkMode: ', prefersDarkMode)
-  // console.log('prefersLightMode: ', prefersLightMode)
-
-  return (
-    <Button onClick={() => {
-      setMode(mode == 'light' ? 'dark' : 'light')
-    }}>
-      {mode == 'light' ? 'Turn dark' : 'Turn Light'}
-    </Button>
-  )
-}
 
 function App() {
   return (
-    <>
-      <ModeSelect></ModeSelect>
-      <hr/>
-      <ModeToggle></ModeToggle>
-      <hr/>
-      <div>Duy Viet</div>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect/>
+      </Box>
 
-      <Typography variant='body2' color="text.secondary">Test Typography</Typography>
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Bar
+      </Box>
 
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
-
-      <HomeIcon color='primary'/>
-      <HomeIcon color='secondary'/>
-      <HomeIcon color='success'/>
-      <HomeIcon color='action'/>
-      <HomeIcon color='disabled'/>
-      <HomeIcon sx={ { color: pink[500] } }/>
-    </>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width: '100%',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board content
+      </Box>
+    </Container>
   )
 }
 
