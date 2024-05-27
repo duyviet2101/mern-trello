a0d33a9307a1174bbff02a2be6c0204104578a4d
duyviet2101 <whoisduyviet@gmail.com>
2024-05-27 23:59:12 +0700

    23.header(3)

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -23,7 +23,8 @@ function AppBar() {
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
       alignItems: 'center',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
+      overflowX: 'auto'
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }}/>
@@ -34,16 +35,19 @@ function AppBar() {
           </Typography>
         </Box>
 
-        <Workspaces/>
-        <Recent/>
-        <Starred/>
-        <Templates/>
+        <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1 }} >
+          <Workspaces/>
+          <Recent/>
+          <Starred/>
+          <Templates/>
+          <Button variant="outlined">Create</Button>
+        </Box>
 
-        <Button variant="outlined">Create</Button>
       </Box>
 
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }} >
-        <TextField id="outlined-search" label="Search..." type="search" size='small'/>
+        <TextField id="outlined-search" label="Search..." type="search" size='small' sx={{ minWidth: '120px' }}/>
+        
         <ModeSelect/>
 
         <Tooltip title="Notification">
